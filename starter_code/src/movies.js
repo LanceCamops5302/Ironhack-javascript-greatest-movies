1d13507ee0312e05a1e877242728254bcee55838
Campos Lance <20058@columbushs.com>
2019-11-20 13:20:20 -0500

    number 3 almost done

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,9 +2,60 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
+function orderByYear(array) {
+    let copy = [...array];
+  
+    copy.sort((a,b)=>{
+      if(a.year < b.year){
+        return -1
+      }
+      else if (b.year < a.year){
+        return 1
+      }
+      else if (a.title < b.title){
+        return -1
+      }
+      else if (b.title < a.title){
+        return 1
+      }
+      else () =>{
+        return 0
+      }
+  })
+  return copy
+  }
+  
+
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
 
+function howManyMovies(anArray) {
+
+
+    let SDrama = anArray.filter((movie)=>{
+      return (movie.director == 'Steven Spielberg' && movie.genre.includes ('Drama'))
+    })
+  
+    return SDrama.length
+  }
+
+
 // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+function orderAlphabetically(order){
+
+    let alph = [...order];
+
+  alph.sort(function (a, b){
+    if (b.title < a.title){
+      return 1
+    }
+    else if (a.title < b.title){
+      return -1
+    }
+  })
+  return alph
+  };
+
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
